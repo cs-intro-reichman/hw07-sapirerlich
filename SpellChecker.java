73743036c861c6cf93fb24e868375afc91a9f604
sapirerlich <sapir.erlich@post.runi.ac.il>
2024-01-29 17:47:19 +0200

    hw7

--- SpellChecker.java
+++ SpellChecker.java
@@ -6,7 +6,6 @@ public class SpellChecker {
 		String word = args[0];
 		int threshold = Integer.parseInt(args[1]);
 		String[] dictionary = readDictionary("dictionary.txt");
-		// System.out.println(levenshtein("hello","hell"));
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
 	}
@@ -16,14 +15,13 @@ public class SpellChecker {
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		if (word1.length()==0  ){
+		if (word1.length()==0){
 			return word2.length();
 		}
-		if (word2.length()==0  ){
+		if (word2.length()==0){
 			return word1.length();
 		}
-		if (word1.charAt(0) == word2.charAt(0)){
-			// System.out.println("index0");
+		if (word1.toLowerCase().charAt(0) == word2.toLowerCase().charAt(0)){
 			return levenshtein(tail(word1),tail(word2));
 		}
 		
@@ -49,12 +47,7 @@ public class SpellChecker {
 		int min =levenshtein(word.toLowerCase(),dictionary[0].toLowerCase());
 		int min_index=0;
 		for (int i=0;i<dictionary.length;i++){ 
-			// System.out.println(i);
-			// System.out.println(levenshtein(word.toLowerCase(),dictionary[i].toLowerCase()));
-			// System.out.println(min);
 			if (levenshtein(word.toLowerCase(),dictionary[i].toLowerCase())<=min){
-				System.out.println(levenshtein(word.toLowerCase(),dictionary[i].toLowerCase()));
-				System.out.println(dictionary[i]);
 				min =Math.min(min,levenshtein(word.toLowerCase(),dictionary[i].toLowerCase()));
 				min_index = i;
 			}
